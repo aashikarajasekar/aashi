ac785763bd0631a5368a78e06786a76f1db5a525
aashikarajasekar <35651549+aashikarajasekar@users.noreply.github.com>
2018-01-21 16:46:07 +0530

    Create greater.c

---
+++ greater.c
@@ -0,0 +1,19 @@
+#include<stdio.h>
+using namespace std;
+ 
+int highestPowerof2(int n)
+{
+
+    int res = 0;
+    for (int i=n; i>=1; i--)
+    {
+        
+        if ((i & (i-1)) == 0)
+        {
+            res = i;
+            break;
+        }
+    }
+    return res;
+}
+ 
