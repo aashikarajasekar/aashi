481f2cd109dd0c0eb927975bd6cbb65a66dd127f
aashikarajasekar <35651549+aashikarajasekar@users.noreply.github.com>
2018-01-31 15:59:15 +0530

    Create 78.c

---
+++ 78.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+void main()
+{
+int n;
+printf("enter the number");
+scanf("%d",&n);
+if(n%13==0)
+{
+printf("it is divisable by 13");
+}
+else
+{
+printf("it is no divisible by 13");
+}
+}
+
