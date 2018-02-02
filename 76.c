aa851512339cc5ed4797e48bf9f4599905b66277
aashikarajasekar <35651549+aashikarajasekar@users.noreply.github.com>
2018-02-02 14:00:43 +0530

    Create 76.c

---
+++ 76.c
@@ -0,0 +1,25 @@
+#include<stdio.h>
+void main()
+{
+    int i,c=0,n;
+    printf("enter the number");
+    scanf("%d",&n);
+    for(i=0;i<=n;i++)
+    {
+        if(n%i==0)
+        {
+            c=c+1;
+        
+        }
+    }
+    if(c==2)
+{
+    printf("it is prime");
+    
+}
+else{
+    printf("it is composite");
+}
+
+}
+
