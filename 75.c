32b0acf3fc815cb9cf8d08fcd58704db0536457d
aashikarajasekar <35651549+aashikarajasekar@users.noreply.github.com>
2018-02-02 14:50:38 +0530

    Create 75.c

---
+++ 75.c
@@ -0,0 +1,29 @@
+#include<stdio.h>
+void main()
+{
+   char s[20]="aashika";int count=0,x,i;char c='*';
+   for(i=0;s[i]!='\0';i++)
+   {
+       count++;
+   }
+   x=(count)/2;
+   if(count%2==0)
+{
+    s[x]=c;
+    s[x+1]=c;
+    for(i=0;s[i]!=0;i++)
+    {
+        printf("%c",s[i]);
+    }
+}
+else
+{
+    s[x]=c;
+    for(i=0;s[i]!=0;i++)
+    {
+        printf("%c",s[i]);
+    }
+    
+}
+   
+}
