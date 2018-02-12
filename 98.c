a1f760be173e365c7ba3bd70e2376a0056ef3b3a
aashikarajasekar <35651549+aashikarajasekar@users.noreply.github.com>
2018-02-12 15:48:06 +0530

    Create 98.c

---
+++ 98.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+void main()
+{
+int pr,r,t,a,b;
+printf("simple ineretst calculation give principle,rate and time");
+scanf("%d%d%d",&pr,&r,&t);
+a=(1+r*t);
+b=pr*a;
+printf("%d", b);
+}
+
