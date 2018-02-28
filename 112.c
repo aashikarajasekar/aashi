35de27befcebe906dc60cff8bf285876ac8a86fe
aashikarajasekar <35651549+aashikarajasekar@users.noreply.github.com>
2018-02-28 15:47:01 +0530

    Create 112.c

---
+++ 112.c
@@ -0,0 +1,28 @@
+#include<stdio.h>
+void main()
+{
+    int a[20],i,count=0,k;
+    printf("enter the elements");
+    for(i=0;i<=5;i++)
+    {
+    scanf("%d",&a[i]);
+    }
+    printf("enter the k element to check");
+    scanf("%d",&k);
+    for(i=0;i<=5;i++)
+    {
+       if(k==a[i])
+       {
+           count++;
+       }
+    }
+    if(count!=0)
+    {
+    printf("yes");
+    }
+    else
+    {
+        printf("no");
+    }
+}
+
